4f44e7c3df6026a610eade2cb469d043230cc92e
Timothy Pranoto <tpranoto@macbook.box>
2021-09-19 21:57:25 +0200

    changed fb dev key

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -1,14 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDlW4jYt4FHYRAnXv6Mc7C6TFq8H4i-Fo4",
-    authDomain: "bundelt-8dc1e.firebaseapp.com",
-    projectId: "bundelt-8dc1e",
-    storageBucket: "bundelt-8dc1e.appspot.com",
-    messagingSenderId: "655768743090",
-    appId: "1:655768743090:web:4e4ed4377239336ad06dab",
-    measurementId: "G-W98FEMN4KX"
-  };
+  apiKey: "AIzaSyCU0CgMza2o13zL4Xtgz_0J11d3MUWivTg",
+  authDomain: "bundelt-411e2.firebaseapp.com",
+  projectId: "bundelt-411e2",
+  storageBucket: "bundelt-411e2.appspot.com",
+  messagingSenderId: "394748186234",
+  appId: "1:394748186234:web:cfbd1cb344afe0ed7635c3"
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
